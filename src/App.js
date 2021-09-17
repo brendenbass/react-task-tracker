71db02268bda0bb68a4796925a72c668540daf08
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2021-09-17 10:00:48 -0400

    Added Task Component; Added react-icons
    - moved state up to app.js level

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import { useState } from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+    id: 1,
+    text: 'Doctor Appointment',
+    day: 'Feb 5 at 2:30 PM',
+    reminder: true,
+    },
+    {
+    id: 2,
+    text: 'Dentist Appointment',
+    day: 'Feb 6 at 2:30 PM',
+    reminder: true,
+    },
+    {
+    id: 3,
+    text: 'School Meeting',
+    day: 'Feb 7 at 2:30 PM',
+    reminder: false,
+    },
+    {
+    id: 4,
+    text: 'City Council',
+    day: 'Feb 8 at 2:30 PM',
+    reminder: true,
+    },
+])
   return (
     <div className="container">
       <Header />
-      <Tasks/>
+      <Tasks tasks={tasks}/>
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,35 +1,10 @@
-const tasks = [
-    {
-    id: 1,
-    text: 'Doctor Appointment',
-    day: 'Feb 5 at 2:30 PM',
-    reminder: true,
-    },
-    {
-    id: 2,
-    text: 'Dentist Appointment',
-    day: 'Feb 6 at 2:30 PM',
-    reminder: true,
-    },
-    {
-    id: 3,
-    text: 'School Meeting',
-    day: 'Feb 7 at 2:30 PM',
-    reminder: false,
-    },
-    {
-    id: 4,
-    text: 'City Council',
-    day: 'Feb 8 at 2:30 PM',
-    reminder: true,
-    },
-]
+import Task from "./Task"
 
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
     return (
         <>
             {tasks.map((task) => (
-            <h3>{task.text}</h3>
+            <Task key={task.id} task={task}/>
             ))}
         </>
     )
