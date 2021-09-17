c3dc6c04931afb8c8179cd2f8da4b3052504f271
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2021-09-17 10:31:06 -0400

    Added toggleReminder state
    - about to pass it down

--- src/App.js
+++ src/App.js
@@ -35,11 +35,17 @@ const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+//Toggle Reminder
+const toggleReminder = (id) => {
+  console.log(id);
+}
+
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? <Tasks tasks={tasks}
       onDelete={deleteTask}
+      onToggle={toggleReminder}
       /> : 'No Tasks to Show'}
     </div>
   )
